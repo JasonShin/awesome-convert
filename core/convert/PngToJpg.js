c7a795a153e11252d44c51fa083403450eb1c604
Jason Shin <visualbbasic@gmail.com>
2017-07-22 15:15:55 +1000

    adding test for png to jpg

--- core/convert/PngToJpg.js
+++ core/convert/PngToJpg.js
@@ -5,13 +5,30 @@ import pngToJpg from 'png-to-jpeg'
 
 const Future = fantasy.Future
 
-const handler = (path) => {
+/**
+ * Converts png to jpg
+ * @param src: path to source
+ * @param dest: path to dest
+ * @returns {*}
+ */
+const handler = (src, dest) => {
   return new Future((rej, res) => {
-    const buffer = fs.readFileSync(path)
-    pngToJpg({ quality: 90 })(buffer)
-      .then(output => {
-        return res(output)
-      })
+    const buffer = fs.readFileSync(src)
+    try {
+      pngToJpg({ quality: 90 })(buffer)
+        .then(output => {
+          if (!dest) {
+            return res(output)
+          } else {
+            const writeResult = fs.writeFileSync(dest, output)
+            return res(writeResult)
+          }
+        })
+    } catch (e) {
+      console.log('failed to create png to jpg!')
+      return rej(e)
+    }
+
   })
 }
 
--- core/tests/unit/convert/PngToJpg.test.js
+++ core/tests/unit/convert/PngToJpg.test.js
@@ -3,12 +3,23 @@ import path from 'path'
 import PngToJpg from '../../../convert/PngToJpg'
 
 describe('png to jpg', () => {
-  it('should create correct file extension', () => {
-    PngToJpg(path.join(__dirname, '../../assets/png-test.png'))
+  it('should create correct file', (done) => {
+    PngToJpg(
+      path.join(__dirname, '../../assets/png-test.png'),
+      path.join(__dirname, '../../assets/png-test.jpg')
+    )
       .fork(console.error, (res) => {
         console.log('success ', res)
-        assert.ok(true)
+        done()
       })
+  })
 
+  it('should not create correct file', (done) => {
+    console.log('testing file should not create')
+    PngToJpg(
+      path.join(__dirname, '../../assets/random-text.txt'),
+      path.join(__dirname, '../../assets/png-test.jpg')
+    )
+      .fork(console.error, console.log)
   })
 })
