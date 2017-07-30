270fe7422a46d3c35ece1d0c211abcfab9439a62
Jason Shin <visualbbasic@gmail.com>
2017-07-30 14:04:40 +1000

    docker path

--- core/convert/HtmlPdf.js
+++ core/convert/HtmlPdf.js
@@ -1,19 +1,24 @@
-import fantasy from 'ramda-fantasy';
-import pdf from 'html-pdf';
+import fantasy from 'ramda-fantasy'
+import pdf from 'html-pdf'
 
-const Future = fantasy.Future;
+const Future = fantasy.Future
 
 const handler = ({
   src,
   dest,
   options = []
 }) => {
-	return new Future((rej, res) => {
-		pdf.create(html, options).toFile(dest, (err, result) => {
-			if (err) {
-				return rej(err);
-			}
-			return res(result);
-		});
-	});
-};
+  return new Future((rej, res) => {
+    pdf.create(html, options).toFile(dest, (err, result) => {
+      if (err) {
+        return rej(err)
+      }
+      return res(result)
+    })
+  })
+}
+
+export default {
+  handler,
+  name: 'htmlToPdf'
+}
